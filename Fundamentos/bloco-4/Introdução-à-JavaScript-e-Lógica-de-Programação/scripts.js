dab0dc62472a4f2b879390e866f8b2f0087ae43d
Harrison <Harrison.couto@outlook.com>
2022-09-21 19:19:15 -0300

    Exercicio2

--- Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
+++ Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
@@ -5,3 +5,9 @@ console.log("Subtração: " + (a - b));
 console.log("Multiplicação: " + (a * b));
 console.log("Divisão: " + (a / b));
 console.log("Modulo: " + (a % b));
+
+if (a > b) {
+    console.log("a e maior que b");
+} else {
+    console.log("b e maior que a");
+}
