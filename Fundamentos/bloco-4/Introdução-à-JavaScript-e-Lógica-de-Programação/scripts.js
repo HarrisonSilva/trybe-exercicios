fa56f617c80d591da1ad1901702dc8c1f4bfb3c0
Harrison <Harrison.couto@outlook.com>
2022-09-21 19:15:36 -0300

    Exercicio1

--- Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
+++ Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
@@ -1,5 +1,7 @@
-const base = 5;
-const height = 8;
-const area = base * height;
-const perimeter = area;
-console.log(perimeter);
+const a = 5;
+const b = 5;
+console.log("Soma: " + (a + b));
+console.log("Subtração: " + (a - b));
+console.log("Multiplicação: " + (a * b));
+console.log("Divisão: " + (a / b));
+console.log("Modulo: " + (a % b));
