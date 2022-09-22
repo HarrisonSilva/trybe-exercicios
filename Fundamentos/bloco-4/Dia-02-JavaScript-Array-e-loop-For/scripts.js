15131ae82b5637ec9154d0c61ed453813c7ecea1
Harrison <Harrison.couto@outlook.com>
2022-09-22 18:56:23 -0300

    exercicio5

--- Fundamentos/bloco-4/Dia-02-JavaScript-Array-e-loop-For/scripts.js
+++ Fundamentos/bloco-4/Dia-02-JavaScript-Array-e-loop-For/scripts.js
@@ -1,13 +1,21 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
-
+let resultado1 = numbers[0];
 for (let i = 0; i < numbers.length; i += 1) {
-    /* console.log(numbers[i]) */
-    resultado += numbers[i];
+   if(numbers[i] > resultado1) {
+    resultado1 = numbers[i];
+   }
 }
+console.log(resultado1)
+   
+   
+    /* console.log(numbers[i]) */
+ /*    resultado += numbers[i]; */
+/* }
 console.log(resultado / numbers.length);
 if (resultado / numbers.length > 20) {
     console.log("valor maior que 20")
 }else if (resultado / numbers.length <= 20) {
     console.log("valor menor ou igual a 20")
-}
+} */
+
