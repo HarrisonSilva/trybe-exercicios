299110843ba01417efb9875158aa7538d0a1f453
Harrison <Harrison.couto@outlook.com>
2022-09-22 23:02:59 -0300

    exercicio6

--- Fundamentos/bloco-4/Dia-02-JavaScript-Array-e-loop-For/scripts.js
+++ Fundamentos/bloco-4/Dia-02-JavaScript-Array-e-loop-For/scripts.js
@@ -1,12 +1,25 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultado = 0;
-let resultado1 = numbers[0];
+let resultado1 = 0;
+/* numbers[0]; */
 for (let i = 0; i < numbers.length; i += 1) {
-   if(numbers[i] > resultado1) {
+   if(numbers[i] % 2 !== 0) {
+     resultado1 += 1;
+   } 
+}
+if (resultado1 === 0) {
+    console.log("nenhum valor impar encontrado")
+} else {
+    console.log(resultado1);
+    
+}
+    
+    
+    /*  > resultado1) {
     resultado1 = numbers[i];
    }
 }
-console.log(resultado1)
+console.log(resultado1) */
    
    
     /* console.log(numbers[i]) */
