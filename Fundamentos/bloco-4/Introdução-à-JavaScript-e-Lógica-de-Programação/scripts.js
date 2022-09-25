4877a4b6f7a380cf9720e54dd1b6a2e90f1c8587
Harrison <Harrison.couto@outlook.com>
2022-09-25 01:39:03 -0300

    exercicio2

--- Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
+++ Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
@@ -1,4 +1,4 @@
-const numero1 = 25;
+/* const numero1 = 25;
 const numero2 = 10;
 
 let adicao = numero1 + numero2;
@@ -11,3 +11,18 @@ console.log(subtracao);
 console.log(multiplicacao);
 console.log(divisao);
 console.log(modulo);
+ */
+
+const numero1 = 7700;
+const numero2 = 9310;
+const numero3 = 10000;
+
+if (numero1 > numero2 && numero1 > numero3) {
+   console.log(numero1, "e Maior que", numero2, "e tambem Maior que", numero3);
+
+} else if (numero2 > numero1 && numero2 > numero3) {
+   console.log(numero2, "e Maior que", numero1, "e tambem Maior que", numero3);
+
+} else if (numero3 > numero1 && numero3 > numero2) {
+   console.log(numero3, "e Maior que", numero1, "e tambem Maior que", numero2);
+}
