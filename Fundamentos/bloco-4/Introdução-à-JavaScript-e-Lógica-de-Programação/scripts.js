a71eec551b23d7692b62756fcd98da72286e0154
Harrison <Harrison.couto@outlook.com>
2022-09-25 01:35:21 -0300

    exercicio1

--- Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
+++ Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
@@ -1,9 +1,13 @@
-let n1 = 1;
-let n2 = 2;
-if(n1 == 2 && n1 == 2) {
-    console.log("negativo");
-} else if(n1 == 1 && n2 == 1) {
-    console.log("Positivo");
-} else {
-    console.log("0");
-}
+const numero1 = 25;
+const numero2 = 10;
+
+let adicao = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+let modulo = numero1 % numero2;
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
