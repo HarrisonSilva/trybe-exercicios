cdf1a7353d6404581134068ef3fe4e4cbc82ad76
Harrison <Harrison.couto@outlook.com>
2022-09-25 02:33:08 -0300

    exercicio6

--- Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
+++ Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
@@ -1,26 +1,16 @@
-let peçaDeXadrez = "jao";
-
-switch(peçaDeXadrez) {
-    case "peao": console.log("peao -> uma ou duas casas para cima");
-    break;
-    case "cavalo": console.log("cavalo -> um L pra qualquer lado");
-    break;
-    case "bispo": console.log("bispo -> diagonal para qualquer lado");
-    break;
-    case "torre": console.log("torre -> todas casas pra cima ou para os lados");
-    break;
-    case "rainha": console.log("qualquer casa pra qualquer lado");
-    break;
-    case "rei": console.log("uma casa pra qualquer lado");
-    break;
-
-    default:
-        console.log("Erro, peça invalida!");
-
-
-
-
-
-
-
-}
+ let nota = 90;
+ if (nota >= 90) {
+    console.log("A");
+ } else if (nota >= 80) {
+    console.log("B");
+ } else if (nota >= 70) {
+    console.log("C");
+ } else if (nota >= 60) {
+    console.log("D");
+ } else if (nota >= 50) {
+    console.log("E");
+ } else if (nota < 50) {
+    console.log("F");
+ } else {
+    console.log("nota invalida");
+ }
