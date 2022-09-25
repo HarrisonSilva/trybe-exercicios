6e2835daf7edfd1059f84f8247b927fb514427c0
Harrison <Harrison.couto@outlook.com>
2022-09-25 18:10:54 -0300

    exercicio7

--- Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
+++ Fundamentos/bloco-4/Introdução-à-JavaScript-e-Lógica-de-Programação/scripts.js
@@ -1,9 +1,9 @@
-const numero1 = 7;
-const numero2 = 11;
-const numero3 = 20;
+const numero1 = 20;
+const numero2 = 6;
+const numero3 = 10;
 let resultado = false;
 
-if (numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0) {
+if (numero1 % 2 === 1 || numero2 % 2 === 1 || numero3 % 2 === 1) {
     resultado = true;
 
 }
