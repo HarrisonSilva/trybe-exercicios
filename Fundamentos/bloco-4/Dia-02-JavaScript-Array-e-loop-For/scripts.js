bcfc838d4aaf1bba6a44d3640fef167b06ef2ce0
Harrison <Harrison.couto@outlook.com>
2022-09-26 21:20:56 -0300

    m

--- Fundamentos/bloco-4/Dia-02-JavaScript-Array-e-loop-For/scripts.js
+++ Fundamentos/bloco-4/Dia-02-JavaScript-Array-e-loop-For/scripts.js
@@ -1,6 +1,10 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-    resultado += numbers[index];
+
+function comparacao(numero1, numero2) {
+  if (numero1 > numero2) {
+  console.log(numero1, "e Maior que", numero2); 
+  } else if (numero2 > numero1) {
+    console.log(numero2, "e Maior que", numero1);
+  }
+
 }
-console.log(resultado);
+console.log(comparacao(5, 10));
