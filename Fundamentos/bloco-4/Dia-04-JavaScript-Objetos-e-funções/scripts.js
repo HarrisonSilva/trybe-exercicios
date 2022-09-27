629f52c9fd4177c0ec4d7dc7058792e67a5f9603
Harrison <Harrison.couto@outlook.com>
2022-09-26 21:26:08 -0300

    exercicio3

--- Fundamentos/bloco-4/Dia-04-JavaScript-Objetos-e-funções/scripts.js
+++ Fundamentos/bloco-4/Dia-04-JavaScript-Objetos-e-funções/scripts.js
@@ -4,4 +4,6 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
     recorrente: 'Sim'
   };
-  console.log(info);
+for (let key in info) {
+    console.log(info);
+}
