0c774ab4a2273bb5d40142c6ca704d4dde32eb5b
Harrison <Harrison.couto@outlook.com>
2022-09-29 21:41:53 -0300

    exercicio3

--- Fundamentos/Secao-5/2-JavaScript-Trabalhando-com-elementos/script.js
+++ Fundamentos/Secao-5/2-JavaScript-Trabalhando-com-elementos/script.js
@@ -3,3 +3,5 @@ const meuElemento = document.getElementById("elementoOndeVoceEsta");
 meuElemento.parentNode.style.color = "pink";
 
 meuElemento.firstElementChild.innerText = "Trybe";
+
+const meuPai = document.getElementById("pai");
