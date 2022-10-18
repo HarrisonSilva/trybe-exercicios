9c65bfa0f3899d6cf6b21511ba319f407888803f
Harrison <Harrison.couto@outlook.com>
2022-10-17 21:07:37 -0300

    exercicio2

--- Fundamentos/Secao-7/1-JavaScript-ES6/script.js
+++ Fundamentos/Secao-7/1-JavaScript-ES6/script.js
@@ -1,14 +1,7 @@
-let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-const testingScope = (escopo) => {
-    if (escopo === true) {
-      ifScope = (`${ifScope} ótimo, fui utilizada no ${escopo} !`);
-      console.log(ifScope);
-   
-    } else {
-      let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-      console.log(elseScope);
-    }
-    console.log(`${ifScope} o que estou fazendo aqui ? :O'`); // Se necessário esta linha pode ser removida.
-  }
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => a - b)
+console.log(oddsAndEvens); 
 
-  testingScope(true);
+
+   
+
