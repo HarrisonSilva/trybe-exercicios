f84f012e3d90ba60d99f140d69109a7748ba6d01
Harrison <Harrison.couto@outlook.com>
2022-11-22 20:14:35 -0300

    exercicio 4

--- Front-End/Secao-2/helloworld/src/About.jsx
+++ Front-End/Secao-2/helloworld/src/About.jsx
@@ -3,16 +3,20 @@ import React from "react";
 class About extends React.Component {
     render() {
         const nome = <h1>Harrison</h1> 
-        const sobre = <p>Estudante de Desenvolvimento</p> 
-        const tecnologias = <ol>
+        const sobre = <p>Estudante de Desenvolvimento</p>
+        const minhashabilidades = <h1>Minhas Habilidades</h1>
+        const tecnologias = <ul>
             <li>Unix</li>
             <li>Bash</li>
             <li>HTML</li>
             <li>Css</li>
             <li>Java Script</li>
-        </ol>
+        </ul>
         return (
-            <header>{nome} {sobre} {tecnologias}</header>
+            <>
+            <header>{nome} {sobre}</header>
+            <section>{minhashabilidades} {tecnologias}</section>
+            </>
         );
         
     }
