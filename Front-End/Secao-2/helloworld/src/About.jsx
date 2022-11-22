ece74a22b8bf08f044946f07bb4dcbf1c51bc37f
Harrison <Harrison.couto@outlook.com>
2022-11-22 20:09:25 -0300

    Exercicio 3

--- Front-End/Secao-2/helloworld/src/About.jsx
+++ Front-End/Secao-2/helloworld/src/About.jsx
@@ -4,8 +4,15 @@ class About extends React.Component {
     render() {
         const nome = <h1>Harrison</h1> 
         const sobre = <p>Estudante de Desenvolvimento</p> 
+        const tecnologias = <ol>
+            <li>Unix</li>
+            <li>Bash</li>
+            <li>HTML</li>
+            <li>Css</li>
+            <li>Java Script</li>
+        </ol>
         return (
-            <header>{nome} {sobre}</header>
+            <header>{nome} {sobre} {tecnologias}</header>
         );
         
     }
