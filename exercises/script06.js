cc22b166a02014c35a88603d0ed4d4574ca2110e
Harrison <Harrison.couto@outlook.com>
2023-07-28 16:31:48 -0300

    Feat: exercise 6 complete

---
+++ exercises/script06.js
@@ -0,0 +1,10 @@
+let value = 5
+let square = ''
+
+for (let i = 1; i <= value; i += 1) {
+    for (let j = 1; j <= i; j += 1) {
+        square += '*'
+    }
+    square += '\n'
+}
+console.log(square);
