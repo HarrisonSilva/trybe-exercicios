b117d1a94bb3667907c3bd7415dca76aff52342d
Harrison <Harrison.couto@outlook.com>
2023-07-27 21:37:27 -0300

    Feat: exercise 2 complete

--- exercises/script00.js
+++ exercises/script00.js
@@ -1,10 +1,3 @@
-/* Utilizando o array abaixo,
-percorra-o somando todos os valores.
-Caso o valor final seja maior que 15,
-imprima-o. Caso seja igual ou menor que 15,
-imprima a mensagem: “valor menor que 16”:
- */
-
 let fruits = [3, 4, 10, 1, 12];
 let sumNumbers = 0
 for (let i = 0; i < fruits.length; i += 1) {
---
+++ exercises/script02.js
@@ -0,0 +1,7 @@
+let word = 'tryber';
+let textBackwards = ''
+
+for (let i = word.length -1; i > - 1; i -= 1) {
+    textBackwards += word[i]
+}
+console.log(textBackwards);
