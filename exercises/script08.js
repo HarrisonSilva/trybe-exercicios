53a1f6c0475a2b6de73147b57803b1e22c2f902d
Harrison <Harrison.couto@outlook.com>
2023-07-30 17:17:24 -0300

    Feat: exercise 8 complete

---
+++ exercises/script08.js
@@ -0,0 +1,17 @@
+let inputLine = '';
+let controlLeft = 3;
+let controlRight = 3;
+
+for (let i = 0; i <= 3; i += 1) {
+  for (let j = 0; j <= 5; j += 1) {
+    if (j > controlLeft && j < controlRight) {
+      inputLine = inputLine + '*';
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
