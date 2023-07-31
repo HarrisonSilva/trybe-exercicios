af3bad1305cc606239fd84e61ecf8ffc9761a83b
Harrison <Harrison.couto@outlook.com>
2023-07-31 20:13:05 -0300

    Feat: exercise 6.5 complete

--- exercises/script06.js
+++ exercises/script06.js
@@ -1,12 +1,12 @@
 let info = {
-    personagem: 'Margarida',
+    personagem: 'Tio Patinhas',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
 console.log('Bem-vinda', info.personagem);
 
-info['recorrente'] = 'sim'
+info['recorrente'] = 'Sim'
 
 console.log(info);
 
@@ -18,3 +18,18 @@ for (let i in info) {
 for (let i in info) {
   console.log(info[i]);
 }
+
+
+let info2 = {
+  personagem: 'Margarida',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorremte: 'Sim'
+};
+
+const person = `${info2.personagem} e ${info.personagem}`
+const origem = `${info.origem} e ${info2.origem}`
+const nota = `${info.nota} e ${info2.nota}`
+const recorrente = info2.recorremte
+
+console.log(person, origem, nota, recorrente);
