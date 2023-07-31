a3421689c38b6c3f9f2fe38104c448d82325f377
Harrison <Harrison.couto@outlook.com>
2023-07-31 16:43:52 -0300

    Feat: exercise 02 complete

---
+++ exercises/script02.js
@@ -0,0 +1,26 @@
+const addition = (a, b) => console.log(a + b)
+addition(10, 10)
+  
+const subtraction = (a, b) => console.log(a + b)
+subtraction(10, 10)
+    
+const multiplication = (a, b) => console.log(a * b)
+multiplication(10, 10)
+    
+const division = (a, b) => console.log(a / b)
+division(10, 10)
+
+const modulo = (a, b) => console.log(a % b)
+modulo(10, 10)
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
