3f86628b0808e3d15634dbd0493d53d6240d0925
Harrison <Harrison.couto@outlook.com>
2023-07-31 16:45:53 -0300

    Feat: exercise 4 complete

---
+++ exercises/script04.js
@@ -0,0 +1,24 @@
+const whatValue = (value) => {
+    if (value > 0) {
+        console.log(value, 'e positivo!');
+    } else if (value < 0) {
+        console.log(value, 'e negativo!');
+    } else {
+        console.log(value, 'e zero!');
+    }
+}
+whatValue(-50)
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
