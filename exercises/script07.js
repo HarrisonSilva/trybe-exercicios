7829970dac04d7e42b194fea8e4abd0db2af1410
Harrison <Harrison.couto@outlook.com>
2023-08-01 19:54:31 -0300

    Feat: exercise 7 complete

---
+++ exercises/script07.js
@@ -0,0 +1,34 @@
+function romanToInteger(roman) {
+    const romanNumerals = {
+      I: 1,
+      IV: 4,
+      V: 5,
+      IX: 9,
+      X: 10,
+      XL: 40,
+      L: 50,
+      XC: 90,
+      C: 100,
+      CD: 400,
+      D: 500,
+      CM: 900,
+      M: 1000,
+    };
+  
+    let result = 0;
+  
+    for (let i = 0; i < roman.length; i += 1) {
+      const currentNumeral = roman[i];
+      const nextNumeral = roman[i + 1];
+
+      if (romanNumerals[currentNumeral] < romanNumerals[nextNumeral]) {
+        result += romanNumerals[nextNumeral] - romanNumerals[currentNumeral];
+        i += 1;
+      } else {
+        result += romanNumerals[currentNumeral];
+      }
+    }
+  
+    return result;
+  }
+  
