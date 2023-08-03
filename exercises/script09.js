ac5206057ccda1c6806e3fea0e1e32aa69333429
Harrison <Harrison.couto@outlook.com>
2023-08-03 19:02:49 -0300

    Feat: exercise 9 compplete

--- exercises/script09.js
+++ exercises/script09.js
@@ -1,40 +1,4 @@
-/* 🚀 Exercício 3
-
-A partir do array de frutas basket, 
-retorne um objeto que contenha o nome
- da fruta como chave e a quantidade de
-  vezes que ela aparece no array como valor.
-   Por exemplo, o array [‘Melancia’, ‘Abacate’, ‘Melancia’,
-     ‘Melancia’, ‘Uva’], deverá retornar
-      { Melancia: 3, Abacate: 1, Uva: 1 }
-       quando passado como argumento para a função.
-
-Em seguida, imprima esse resultado na tela
- com uma mensagem no seguinte formato:
-  Sua cesta possui: x Melancias, x Abacates...
-
-const basket = [
-  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
-  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
-  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
-  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
-  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
-  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
-  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
-  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
-  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
-  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
-  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
-  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
-  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
-  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
-  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
-  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
-  'Banana', 'Pera', 'Abacate', 'Uva',
-];
- */
-
-const iFruts = (fruit) => {
+const iFruts = ( ...fruit) => {
     const basket = [
         'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
         'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
@@ -54,19 +18,32 @@ const iFruts = (fruit) => {
         'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
         'Banana', 'Pera', 'Abacate', 'Uva',
       ];
-      let quantity = 0
-      for (let i = 0; i < basket.length; i += 1) {
-        if (fruit === basket[i]) {
-            quantity += 1
-        }
-      }
 
       const newFruits = {}
-      newFruits[fruit] = quantity
-      return newFruits
+      let quantity = 0
+
+      for (let i = 0; i < fruit.length; i += 1) {
+          for (let j = 0; j < basket.length; j += 1) {
+            if (fruit[i] === basket[j]) {
+                quantity += 1
+            }
+          }
+          // newFruits {} recebe fruit no I que e 'Melancia' e 'Jaca' do parametro 
+          //fruit
+          newFruits[fruit[i]] = quantity
+
+      }
+      let myBasket = []
+      console.log('Sua cesta possui:');
+      for (let [fruit, quantity] of Object.entries(newFruits)) {
+        myBasket.push([quantity, fruit])
+      }
+      for (let x in myBasket) {
+        let basket = myBasket[x]
+        for (let xx = 0; xx < basket.length; xx += 1) {
+          console.log(basket[xx]);
+        }
+      }
 }
 
-
-console.log(iFruts('Melancia'));
-
-
+iFruts('Uva');
