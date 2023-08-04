50f9bb30262fb287a35825404f17548792e3789f
Harrison <Harrison.couto@outlook.com>
2023-08-04 17:15:51 -0300

    Feat: exercise 2 complete

---
+++ exercises/script02.js
@@ -0,0 +1,15 @@
+  let favoriteFood = 'Lasanha';
+  favoriteFood = 'Hamburguer';
+  console.log(favoriteFood);
+
+  const nome = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Olá ${nome} ${lastName}!`);
+
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
