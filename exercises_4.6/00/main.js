4bcfc2c6f3c021809d5905db603d1532e53b073e
Harrison <Harrison.couto@outlook.com>
2023-08-07 22:30:51 -0300

    Feat: exercise 0 complete

--- exercises_4.6/00/main.js
+++ exercises_4.6/00/main.js
@@ -4,18 +4,28 @@ const thirdLi = document.getElementById('third-li');
 const myWebpage = document.getElementById('my-spotrybefy');
 const input = document.getElementById('input');
 
-const handleChangeTech = (event) => {
-    const techElement = document.querySelector('.tech');
-    techElement.classList.remove('tech');
-    event.target.classList.add('tech');
-    input.value = '';
-  }
+const manipulatClassTech = (event) => {
+    const tech = document.querySelector(".tech")
+    tech.classList.remove('tech')
+    event.target.classList.add('tech')
+    input.value = ''
+}
 
-firstLi.addEventListener('click', handleChangeTech);
-secondLi.addEventListener('click', handleChangeTech);
-thirdLi.addEventListener('click', handleChangeTech);
+firstLi.addEventListener("click", manipulatClassTech)
+secondLi.addEventListener("click", manipulatClassTech)
+thirdLi.addEventListener("click", manipulatClassTech)
 
 input.addEventListener('input', (event) => {
-    const techElement = document.querySelector('.tech');
-    techElement.innerText = event.target.value;
-  });
+    const tech = document.querySelector('.tech')
+    tech.innerText = event.target.value
+})
+
+
+myWebpage.addEventListener("dblclick", () => {
+    window.location.replace('https://www.betrybe.com/')
+})
+
+myWebpage.addEventListener('mouseover', () => {
+    myWebpage.style.color = 'red'
+})
+
