2375955beab94f7b713e736fb8ac0c0e284e0fce
Harrison <Harrison.couto@outlook.com>
2023-08-28 20:24:11 -0300

    Feat: exercise 00 complete

---
+++ exercises_5.2/00/script.js
@@ -0,0 +1,14 @@
+const pessoasAprovadas = ['Ana Beatriz', 'Caio Nunes', 'Afonso Ribeiro', 'Leonardo Lins'];
+pessoasAprovadas.forEach((nome) => console.log(nome.toLocaleUpperCase()))
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.forEach((element) => console.log(element * 2));
+
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+
+  emailListInData.forEach((email) => console.log(`O email ${email} está cadastrado em nosso banco de dados!`))
