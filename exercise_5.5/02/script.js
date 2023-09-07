afd974ae68a3ac91a649656d796cb460da4eb84b
Harrison <Harrison.couto@outlook.com>
2023-09-07 19:25:36 -0300

    Feat: exercise 02 complete

---
+++ exercise_5.5/02/script.js
@@ -0,0 +1,9 @@
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rec) => console.log(rectangleArea(rec[0], rec[1])))
