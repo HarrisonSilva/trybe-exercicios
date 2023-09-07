573bea2d88fd3ad91dc3d942e6a17057d22c580d
Harrison <Harrison.couto@outlook.com>
2023-09-07 20:31:00 -0300

    Feat: exercise 00 complete

---
+++ exercise_5.6/00/script.js
@@ -0,0 +1,10 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+const [ seventeen, twentyThree, thirtySeven] = primeNumbers
+
+sum(thirtySeven, twentyThree)
+
