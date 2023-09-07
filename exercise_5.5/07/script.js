0d1dc734143c02e9784cb08955b7584fa58df0d7
Harrison <Harrison.couto@outlook.com>
2023-09-07 20:20:59 -0300

    Feat: exercise 07 complete

---
+++ exercise_5.5/07/script.js
@@ -0,0 +1,8 @@
+
+const yearSeasons = {
+    spring: ['March', 'April', 'May'],
+    summer: ['June', 'July', 'August'],
+    autumn: ['September', 'October', 'November'],
+    winter: ['December', 'January', 'February'],
+  };
+  const MonthsOfTheYear = [ ...yearSeasons.spring, ...yearSeasons.summer, ...yearSeasons.autumn, ...yearSeasons.winter]
